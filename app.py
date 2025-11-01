79f256c4704b26c0aebf0d7a17d5b20502214f82
Rayssa <rayssarrsilva1@gmail.com>
2025-11-01 18:49:25 -0300

    ajustes na logica do app.py e funcoes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from mat2 import processar_matriz
 from matriz import matriz_user
+from menor import menor_valor
 
 
 app = Flask(__name__)
@@ -20,14 +21,25 @@ def resultado():
     diagonal, negativos = processar_matriz(n, valores)
     return render_template("resultado.html", diagonal=diagonal, negativos=negativos, n=n)
 
-@app.route("/matriz", methods=["POST"])
-def matriz_route():
-    linhas = int(request.form["linhas"])
-    colunas = int(request.form["colunas"])
-
-    matriz = matriz_user(linhas, colunas, request.form)
-    return render_template("mostrar_matriz.html", matriz=matriz, linhas=linhas, colunas=colunas)
-
+@app.route("/matriz2", methods=["POST"])
+def matriz2_route():
+    if "valor_0_0" in request.form:  # já veio com os valores da matriz
+        linhas = int(request.form["linhas"])
+        colunas = int(request.form["colunas"])
+        matriz = matriz_user(linhas, colunas, request.form)
+        return render_template("mostrar_matriz2.html", matriz=matriz, linhas=linhas, colunas=colunas)
+    else:  # veio só com linhas e colunas
+        linhas = int(request.form["linhas"])
+        colunas = int(request.form["colunas"])
+        return render_template("matriz2.html", linhas=linhas, colunas=colunas)
+    
+@app.route("/menor", methods=["GET", "POST"])
+def menor_route():
+    if request.method == "POST":
+        menor = menor_valor(request.form)
+        return render_template("mostrar_menor.html", menor=menor)
+    return render_template("index.html")
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
 
